57ac1779851799a4d7667eb83697f3ac2f8e0a73
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-16 00:00:02 -0700

    betty comments wahala

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * multi
+ * line
+ * comment
+ */
 
-int main(void) /* control fn */
+int main(void) 
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
