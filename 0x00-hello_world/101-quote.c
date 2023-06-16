6d3f1c72fe5c4846bb4e43a4fe4e2e8685d468e0
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-15 21:51:32 -0700

    stderr print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h> 
+/*
+ * Write a C program that prints exactly and that piece of art is useful"
+ *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
+	return (1);
+}
