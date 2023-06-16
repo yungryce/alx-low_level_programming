11f14b10f75898c8343a5313d9db9c8fe560c48c
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-15 22:17:38 -0700

    betty edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
+/**
  * Write a C program that prints exactly and that piece of art is useful"
  *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
  */
