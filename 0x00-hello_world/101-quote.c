f88dfc77deff188f878eb8191e62411fb1d38842
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-15 23:55:32 -0700

    betty comments wahala

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
+/* Write a C program followed by a new line, to the standard error. */
+
 #include <stdio.h>
 #include <unistd.h>
 
-/**
- * Write a C program followed by a new line, to the standard error.
- */
-
-int main(void)
 /* control fn */
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
