9c52e920df9b475457f129448d72c16283cf3176
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-16 21:51:22 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* Looping through small letters and printing  
+* with putchar()
+*/
+
+int main(void)
+{
+       char ch;
+
+       for (ch = 'a' ; ch <= 'z' ; ch++)
+       {
+       putchar(ch);
+       }
+       putchar('\n');
+
+       return (0);
