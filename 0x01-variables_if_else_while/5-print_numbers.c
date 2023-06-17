f6ab698bc56a6afa242937bad814d2c0f5e1b9af
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-17 01:10:36 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers
+ * Return: 0 to main
+ */
+int main(void)
+{
+	int counter;
+
+	for (counter = 0; counter <= 9; counter++)
+	{
+		printf("%i\n", counter);
+	}
+
+	return (0);
+}
