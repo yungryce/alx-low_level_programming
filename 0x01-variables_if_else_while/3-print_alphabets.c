ebc695a8a2b75b974d1ffebce3e196530a26858a
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-17 00:41:38 -0700

    upper and lower

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int x;
 
-	for (x = a; x <= z; x++)
+	for (x = 'a'; x <= 'z'; x++)
 		putchar(x);
-	for (x = A; x <= Z; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 		putchar(x);
 	putchar('\n');
 
