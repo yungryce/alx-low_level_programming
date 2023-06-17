9b11bc7fe99f89a0bf02fe1bf5855634f574f067
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-17 00:37:46 -0700

    upper and lower

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - prints in upper and lower using putchar
+ *Return: 0 value to fn main
+ */
+int main(void)
+{
+	int x;
+
+	for (x = a; x <= z; x++)
+		putchar(x);
+	for (x = A; x <= Z; x++)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
