6eab3c68eeeb9809ffb399c2c51ae6d67b9b6e1b
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-21 08:24:26 -0700

    9 times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,17 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
+
+		_putchar('\n');
+
 		for (y = 0; y <= 9; y++)
 		{
-			z = y * x;
+
+			z = x * y;
+
 			if (z < 10)
 			{
+				_putchar(' ');
 				_putchar(z + '0');
 			}
 			else
@@ -23,9 +29,12 @@ void times_table(void)
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
 			}
-			if (z != 81)
-			_putchar(',');
-			_putchar(' ');
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
 		}
 	}
 	_putchar('\n');
