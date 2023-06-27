13a138344dc782c5d97802046b4fc7a62cb8a46b
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-06-26 20:10:02 -0700

    changing a variable value usinga pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointer updates the value of the to 98
+ * @n: pointer to the variable to update
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 #define MAIN_H
 
+int _putchar(char c);
 void reset_to_98(int *n);
