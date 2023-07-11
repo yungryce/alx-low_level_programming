c9daf9fba34bc1a874af6b582e1b8dfb05b5666f
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-07-11 19:57:29 +0100

    function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,6 @@ char *str_concat(char *s1, char *s2)
 	char *n;
 	int i, j, len, len1 = 0, len2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
