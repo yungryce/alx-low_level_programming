ddcdf0802a086ce30ce91889c63c4ad37ea5e88d
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-07-20 06:08:16 +0100

    calculator program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,12 +20,14 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 5; i++)
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
+
+		i++;
 	}
 
 	return (NULL);
