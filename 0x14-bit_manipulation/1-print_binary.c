9c90a18bf40a05a8a538c340b2d1d0b40ed39981
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-08-03 19:32:29 +0100

    function that returns the value of a bit at a given index

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,16 +14,18 @@ void print_binary(unsigned long int n)
 	unsigned long int temp = n;
 
 	if (n == 0)
-		printf("0");
-
+	{
+		_putchar('0');
+		return;
+	}
 	for (; (temp >>= 1) > 0; i++)
 		;
 	for (; i >= 0; i--)
 	{
 		if ((n >> i) & 1)
-			printf("1");
+			_putchar('1');
 		else
-			printf("0");
+			_putchar('0');
 	}
 }
 
