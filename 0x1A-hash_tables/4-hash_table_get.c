378a321fa643bf86421fc72bb75002b41d2e12d1
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-11-30 14:56:49 +0100

    >>> print(ht)

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,8 +13,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *temp;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
+	if (!ht || !ht->array || !ht->size || !key || *key == '\0')
+		return (NULL);
 
 	temp = ht->array[index];
 	while(temp)
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ *
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int flag = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
