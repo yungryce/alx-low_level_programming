8c42d01a2f0e4bbf173982d23b0faa4254b627f5
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-01 10:16:53 +0100

    $ht['Betty'] = 'Cool'"

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -82,4 +82,13 @@ typedef struct shash_table_s
 } shash_table_t;
 
 
+shash_table_t *shash_table_create(unsigned long int size);
+shash_node_t *make_shash_node(const char *key, const char *value);
+void add_to_sorted_list(shash_table_t *table, shash_node_t *node);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
 #endif
