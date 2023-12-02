15dea92b66de3fc3208ba0ba40b1495a87929f26
Chigbu Joshua <chigbujoshua@yahoo.com>
2023-12-02 04:20:32 +0100

    >>> ht['betty']"

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,6 +16,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
 	temp = ht->array[index];
 	while (temp)
